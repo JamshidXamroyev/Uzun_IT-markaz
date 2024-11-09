c97d952b8c5f42400f4d531a7b45e6dacedf1e3c
JamshidXamroyev <frontend.dev0811@gmail.com>
2024-11-09 19:26:45 +0500

    Uzun IT markaz yangi version

--- src/component/teacherCard.jsx
+++ src/component/teacherCard.jsx
@@ -3,12 +3,12 @@ import {teacher} from '../util/constants'
 const Billing = () => {
   return (
     <>
-     <h1 className='font-bold text-center text-white pl:text-7xl text-4xl py-10 font-titleFont'>Bizning O'qituvchilar</h1>
+     <h1 className='font-bold text-center text-white pl:text-7xl text-4xl py-10 font-titleFont'>Bizning Kurslar</h1>
       <section id="product" className="flex pl:justify-around justify-start items-center pl:flex-row flex-col flex-wrap">
         {teacher.map(item => (
           <div className="flex pl:justify-center justify-start items-center flex-wrap flex-col pl:w-[24vw] pl:m-2 my-6 border pl:p-0 p-2 pl:h-[55vh]" key={item.id}>
             <div className='pl:w-[200px] w-[70vw] pl:h-[40vh] pl:p-0 p-6 flex justify-center items-center'>
-              <img src={item.img} alt="Image" className='w-full rounded-lg duration-500 pl:hover:translate-y-[-20px] hover:translate-y-[-10px]'/>
+              <img src={item.img} alt="logo" className='w-full rounded-lg duration-500 pl:hover:translate-y-[-20px] hover:translate-y-[-10px]'/>
             </div>
             <h2 className='text-white font-semibold font-sans text-[20px] my-1'>O'qituvchi: {item.title}</h2>
             <div className="flex justify-between items-center border-t w-full px-6 h-[10%]">
